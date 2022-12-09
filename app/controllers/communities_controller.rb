6a1a3f9015d7357d9a1ed630bd091cc743b02c8f
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-09 18:32:09 +0100

    Fix signup create post

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -9,6 +9,7 @@ class CommunitiesController < ApplicationController
   def show
     @posts = @community.posts
     @subscriber_count = @community.subscribers.count
+    @is_subscribed = account_signed_in? ? Subscription.where(community_id: @community.id, account_id: current_account.id).any? : false
   end
 
   def new
