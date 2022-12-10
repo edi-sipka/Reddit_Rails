a9ac2ea1584906f979ffb15df8c40ca1f942fe2d
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-10 16:53:45 +0100

    Fix migration bug

--- db/migrate/20221210153427_create_comments.rb
+++ db/migrate/20221210153427_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :message
-      t.reference :account
-      t.reference :post 
+      t.references :account
+      t.references :post 
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_145235) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_153427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_145235) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.bigint "account_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_comments_on_account_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "communities", force: :cascade do |t|
