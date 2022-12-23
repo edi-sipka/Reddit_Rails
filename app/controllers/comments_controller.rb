7a8a748e3f76b86e74581e46b5a918fae9aa70db
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-23 11:37:13 +0100

    Fix bugs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,13 @@
 class CommentsController < ApplicationContoller
-
+  def new
+    @comments = Comment.new
+  end
+  
   def create
     @comment = Comment.new comment_params
     @comment.account_id = current_account.id 
 
-    respond_to do |format|
-      format.js {
-        if @comment.save
-          @comments = Comment.where(post_id: @comment.post_id)
-          render "comments/create"
-        else
-        end
-      }
-    end
+    
   end
 
   def comment_params
